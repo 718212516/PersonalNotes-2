978afe2496cbe786a7769838f6bf4369ce9bfcee
Admin <admin@admins-mbp.attlocal.net>
2016-11-27 19:03:57 -0600

    Initial commit

---
+++ app/src/main/java/com/mperez16/personalnotes/HelpFeedActivity.java
@@ -0,0 +1,15 @@
+package com.mperez16.personalnotes;
+
+import android.os.Bundle;
+
+
+public class HelpFeedActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_feedback_layout);
+        mToolBar = activateToolbar();
+        setUpNavigationDrawer();
+    }
+}
---
+++ app/src/main/java/com/mperez16/personalnotes/NavigationDrawerItem.java
@@ -0,0 +1,19 @@
+package com.mperez16.personalnotes;
+
+public class NavigationDrawerItem {
+    private int iconId;
+    private String title;
+
+    public NavigationDrawerItem(int iconId, String title) {
+        this.iconId = iconId;
+        this.title = title;
+    }
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
